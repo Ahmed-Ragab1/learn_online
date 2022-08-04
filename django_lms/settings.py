e75d4322abf1196aeee0881d788fb924d734023b
Ahmed-Ragab1 <ahmed.ragab11498@gmail.com>
2022-08-04 23:51:12 +0200

    teacher registration

--- django_lms/settings.py
+++ django_lms/settings.py
@@ -111,10 +111,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+#
 # REST_FRAMEWORK={
 #     'DEFAULT_AUTHENTICATION_CLASSES':[
-#         # 'rest_framework.authentication.TokenAuthentication'
+#         'rest_framework.authentication.TokenAuthentication'
 #     ],
 # }
 
--- lms_frontend/src/components/teacher/TeacherRegister.js
+++ lms_frontend/src/components/teacher/TeacherRegister.js
@@ -1,52 +1,98 @@
 import {NavLink} from 'react-router-dom';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+const baseUrl='http://127.0.0.1:8000/api/teacher/';
 
 function TeacherRegister(){
-    useEffect(() => {
-        document.title = 'Make email to LMS';
-      });
+    const [teacherData,setteacherData]=useState({
+        'full_name':'',
+        'eamil':'',
+        'password':'',
+        'mobile_no':'',
+        'qualification':'',
+        'skills':'',
+        'status':'',
+    });
+
+    const handleChange=(event)=>{
+        setteacherData({
+            ...teacherData,
+            [event.target.name]:event.target.value
+        });
+    }
+
+
+    const submitForm=()=>{
+        const teacherFormData=new FormData();
+        teacherFormData.append('full_name',teacherData.full_name)
+        teacherFormData.append('eamil',teacherData.eamil)
+        teacherFormData.append('password',teacherData.password)
+        teacherFormData.append('mobile_no',teacherData.mobile_no)
+        teacherFormData.append('qualification',teacherData.qualification)
+        teacherFormData.append('skills',teacherData.skills)
+
+        
+
+        try{
+            axios.post(baseUrl,teacherFormData).then((response)=>{
+                console.log(response.data);
+                setteacherData({
+                    'full_name':'',
+                    'eamil':'',
+                    'password':'',
+                    'mobile_no':'',
+                    'qualification':'',
+                    'skills':'',
+                    'status':'success',
+                });
+            });
+        }catch(error){
+            setteacherData({'status':'error'})
+        }
+    };
+  
     return(
         <div className='container mt-4'>
             <div className='row'>
                 <div className='col-6 offset-3'>
+                    {teacherData.status == 'success' && <p className='text-success'>Thanks for Your Registration</p>}
+                    {teacherData.status == 'error' && <p className='text-danger'>Somthing Wrong Happened!</p>}
                     <div className='card'>
                         <h5 className='card-header'>Teacher Register</h5>
                         <div className='card-body'>
-                            <form>
                                 <div className='mb-3'>
                                     <label for='exampleInputEmail' className='form-label'>Full Name</label>
-                                    <input type='text' className='form-control'/>
+                                    <input onChange={handleChange} value={teacherData.full_name} name='full_name' type='text' className='form-control'/>
                                 </div>
 
                                 <div className='mb-3'>
                                     <label for='exampleInputEmail' className='form-label'>Email</label>
-                                    <input type='email' className='form-control'/>
+                                    <input onChange={handleChange} value={teacherData.eamil} name='eamil' type='email' className='form-control'/>
                                 </div>
 
                                 <div className='mb-3'>
                                     <label for='exampleInputPassword1' className='form-label'>Password</label>
-                                    <input type='password' className='form-control' id='exampleInputPassword1'/>
+                                    <input onChange={handleChange} value={teacherData.password} name='password' type='password' className='form-control' id='exampleInputPassword1'/>
                                 </div>
 
 
                                 <div className='mb-3'>
                                     <label for='exampleInputEmail' className='form-label'>mobile number</label>
-                                    <input type='number' className='form-control'/>
+                                    <input onChange={handleChange} value={teacherData.mobile_no} name='mobile_no' type='number' className='form-control'/>
                                 </div>
 
                                 <div className='mb-3'>
                                     <label for='exampleInputEmail' className='form-label'>qualifications</label>
-                                    <input type='text' className='form-control'/>
+                                    <input onChange={handleChange} value={teacherData.qualification} name='qualification' type='text' className='form-control'/>
                                 </div>
                                 
                                 <div className='mb-3'>
                                     <label for='exampleInputPassword1' className='form-label'>Skills</label>
-                                    <textarea className='form-control'></textarea>
+                                    <textarea onChange={handleChange} value={teacherData.skills} className='form-control' name='skills'></textarea>
                                     <div className='form-text'>html, css, js, etc</div>
                                 </div>
 
-                                <button type='submit' className='btn btn-primary'>Sign Up</button>
-                            </form>
+                                <button onClick={submitForm} type='submit' className='btn btn-primary'>Sign Up</button>
                         </div>
                     </div>
                 </div>
